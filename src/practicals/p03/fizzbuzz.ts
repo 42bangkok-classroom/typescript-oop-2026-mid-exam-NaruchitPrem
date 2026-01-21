55c6932a19f1a1bd1fc747e59ab690bdbcc4e681
Naruchit Choenghom <68010578@kmitl.ac.th>
2026-01-21 14:33:54 +0700

    p03

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,24 @@
 // Write your code below
+let input = +process.argv[2];
+
+if (process.argv[2] === "") {
+  console.log('Invalid Input');
+  process.exit();
+}
+
+if (Number.isNaN(input) || input <= 0) {
+    console.log('Invalid Input');
+    process.exit();
+}
+
+for (let i = 1; i <= input; i++){
+  if (i % 5 === 0 && i % 3 === 0){
+    console.log('FizzBuzz');
+  }else if (i % 3 === 0){
+    console.log('Fizz');
+  }else if (i % 7 === 0){
+    console.log('Buzz');
+  }else if (!(i % 3 === 0 || i % 7 === 0)){
+    console.log(`${i}`);
+  }
+}
