47e0c6f48a79e07ec7e10a883ada02feaae0721b
Naruchit Choenghom <68010578@kmitl.ac.th>
2026-01-21 15:40:12 +0700

    check

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -3,7 +3,6 @@ export{}
 let input = +process.argv[2];
 
 if (Number.isNaN(input) || input <= 0){
-    console.log('Invalid Input');
 }else for (let i = 1; i <= input; i++){
     let result = ''
     for (let x = input - i; x > 0; x --){
