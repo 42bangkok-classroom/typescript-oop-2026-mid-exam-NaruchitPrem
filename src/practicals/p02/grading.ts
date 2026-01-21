5734da2bc7885e684974c0aebf7c4256df6dd3da
Naruchit Choenghom <68010578@kmitl.ac.th>
2026-01-21 14:27:37 +0700

    choices

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,26 @@
 // Write your code below
+const score = +process.argv[2]
+
+if (isNaN(score)) {
+    console.log ('Invalid input');
+    process.exit()
+}
+
+if (score > 100 || score < 0) {
+    console.log ('Invalid input');
+    process.exit()
+}
+
+if (score >= 80 ) {
+    console.log('A');
+}else if (score >= 70) {
+    console.log('B');
+}else if (score >= 60) {
+    console.log('C');
+}else if (score >= 50) {
+    console.log('D');
+}else if (score < 50) {
+    console.log('F');
+}else {
+    console.log('Invalid input');
+}
