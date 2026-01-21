3f9197f86f0df30af5b962ebab4de78ade9c623f
Naruchit Choenghom <68010578@kmitl.ac.th>
2026-01-21 15:29:49 +0700

    check

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -5,21 +5,17 @@ let input = +process.argv[2];
 if (process.argv[2] === "") {
   console.log('Invalid Input');
   process.exit();
-}
-
-if (Number.isNaN(input) || input <= 0) {
+}else if (Number.isNaN(input) || input <= 0) {
     console.log('Invalid Input');
     process.exit();
-}
-
-for (let i = 1; i <= input; i++){
-  if (i % 5 === 0 && i % 3 === 0){
-    console.log('FizzBuzz');
-  }else if (i % 3 === 0){
-    console.log('Fizz');
-  }else if (i % 7 === 0){
-    console.log('Buzz');
-  }else if (!(i % 3 === 0 || i % 7 === 0)){
-    console.log(`${i}`);
+}else for (let i = 1; i <= input; i++){
+    if (i % 5 === 0 && i % 3 === 0){
+      console.log('FizzBuzz');
+    }else if (i % 3 === 0){
+      console.log('Fizz');
+    }else if (i % 7 === 0){
+      console.log('Buzz');
+    }else if (!(i % 3 === 0 || i % 7 === 0)){
+      console.log(`${i}`);
+    }
   }
-}
--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -3,16 +3,14 @@ export{}
 let input = +process.argv[2];
 
 if (Number.isNaN(input) || input <= 0){
-  process.exit()
-}
-
-for (let i = 1; i <= input; i++){
-  let result = ''
-  for (let x = input - i; x > 0; x --){
-    result += ' '
+    console.log('Invalid Input');
+}else for (let i = 1; i <= input; i++){
+    let result = ''
+    for (let x = input - i; x > 0; x --){
+      result += ' '
+    }
+    for (let j = i; j > 0; j--){
+      result += j
+    }
+    console.log(result)
   }
-  for (let j = i; j > 0; j--){
-    result += j
-  }
-  console.log(result)
-}
