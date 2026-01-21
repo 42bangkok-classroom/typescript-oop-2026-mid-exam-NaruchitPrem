4abeca041726b54f315b79317b3865a6caf878a4
Naruchit Choenghom <68010578@kmitl.ac.th>
2026-01-21 16:43:38 +0700

    p0ุ

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,7 +1,18 @@
 export function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
   // Write your code below
-  
-  return [1,0]
+  const all = [...arr1, ...arr2];
+
+  const result: number[] = [];
+
+  for (const num of all) {
+    const count = all.filter((n) => n === num).length;
+
+    if (count === 1) {
+      result.push(num);
+    }
+  }
+
+  return result;
 }
 
 const arr1 = [1, 2, 3, 4];
